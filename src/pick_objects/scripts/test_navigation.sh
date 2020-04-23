1e50f889731ec3c92e1d7ffca4f748b06ea2579d
fantauzzi <Francesco_Fantauzzi@yahoo.com>
2020-04-23 16:15:46 +0200

    Implemented shell scripts to start mapping and navigation

---
+++ src/pick_objects/scripts/test_navigation.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+PICK_OBJECTS=$(rospack find pick_objects)
+
+PICK_OBJECTS_WORLD=${PICK_OBJECTS}/worlds/training_world.world
+xterm +hold -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${PICK_OBJECTS_WORLD}" &
+sleep 2
+
+MAP_FILE=${PICK_OBJECTS}/maps/training_world.yaml
+xterm +hold -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=${MAP_FILE} initial_pose_a:=-1.5708" &
+sleep 2
+
+xterm +hold -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
--- src/pick_objects/scripts/test_slam.sh
+++ src/pick_objects/scripts/test_slam.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
-# xterm +hold -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
-xterm +hold -e "roslaunch pick_objects world.launch" &
+PICK_OBJECTS=$(rospack find pick_objects)
+
+PICK_OBJECTS_WORLD=${PICK_OBJECTS}/worlds/training_world.world
+xterm +hold -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${PICK_OBJECTS_WORLD}" &
 sleep 2
+
 xterm +hold -e "roslaunch turtlebot_teleop keyboard_teleop.launch" &
 sleep 2
+
 xterm +hold -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
 sleep 2
+
 xterm +hold -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &

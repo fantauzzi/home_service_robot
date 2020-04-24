5930c53dedcdccd67c5de511ea3fb4ef40411f58
fantauzzi <Francesco_Fantauzzi@yahoo.com>
2020-04-24 11:19:23 +0200

    Added script to add cube marker and moved scripts to a different directory

--- src/add_markers/src/add_markers.cpp
+++ src/add_markers/src/add_markers.cpp
@@ -1,5 +1,65 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
+#include <tf2/LinearMath/Quaternion.h>
+
+void display_cube(ros::Publisher & marker_pub, double x, double y, double theta) {
+    uint32_t shape = visualization_msgs::Marker::CUBE;
+    double side = .5;
+    visualization_msgs::Marker marker;
+    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+    marker.header.frame_id = "/map";
+    marker.header.stamp = ros::Time::now();
+
+    // Set the namespace and id for this marker.  This serves to create a unique ID
+    // Any marker sent with the same namespace and id will overwrite the old one
+    marker.ns = "add_markers";
+    marker.id = 0;
+
+    // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
+    marker.type = shape;
+
+    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+    marker.action = visualization_msgs::Marker::ADD;
+
+    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+    marker.pose.position.x = x;
+    marker.pose.position.y = y;
+    marker.pose.position.z = side/2;
+
+    tf2::Quaternion orientation;
+    orientation.setRPY( 0, 0, theta );
+
+    marker.pose.orientation.x = orientation.getX();
+    marker.pose.orientation.y = orientation.getY();
+    marker.pose.orientation.z = orientation.getZ();
+    marker.pose.orientation.w = orientation.getW();
+
+    // Set the scale of the marker -- 1x1x1 here means 1m on a side
+    marker.scale.x = side;
+    marker.scale.y = side;
+    marker.scale.z = side;
+
+    // Set the color -- be sure to set alpha to something non-zero!
+    marker.color.r = 0.0f;
+    marker.color.g = 1.0f;
+    marker.color.b = 0.0f;
+    marker.color.a = 1.0;
+
+    marker.lifetime = ros::Duration();
+
+    // Publish the marker
+    while (marker_pub.getNumSubscribers() < 1)
+    {
+        if (!ros::ok())
+        {
+            ROS_ERROR("Error while waiting for a subscriber to the marker");
+        }
+        ROS_WARN_ONCE("Please create a subscriber to the marker");
+        sleep(1);
+    }
+    marker_pub.publish(marker);
+
+}
 
 int main( int argc, char** argv )
 {
@@ -8,78 +68,5 @@ int main( int argc, char** argv )
     ros::Rate r(1);
     ros::Publisher marker_pub = n.advertise<visualization_msgs::Marker>("visualization_marker", 1);
 
-    // Set our initial shape type to be a cube
-    uint32_t shape = visualization_msgs::Marker::CUBE;
-
-    while (ros::ok())
-    {
-        visualization_msgs::Marker marker;
-        // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-        marker.header.frame_id = "/map";
-        marker.header.stamp = ros::Time::now();
-
-        // Set the namespace and id for this marker.  This serves to create a unique ID
-        // Any marker sent with the same namespace and id will overwrite the old one
-        marker.ns = "add_markers";
-        marker.id = 0;
-
-        // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-        marker.type = shape;
-
-        // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-        marker.action = visualization_msgs::Marker::ADD;
-
-        // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-        marker.pose.position.x = 0;
-        marker.pose.position.y = 0;
-        marker.pose.position.z = 0;
-        marker.pose.orientation.x = 0.0;
-        marker.pose.orientation.y = 0.0;
-        marker.pose.orientation.z = 0.0;
-        marker.pose.orientation.w = 1.0;
-
-        // Set the scale of the marker -- 1x1x1 here means 1m on a side
-        marker.scale.x = 1.0;
-        marker.scale.y = 1.0;
-        marker.scale.z = 1.0;
-
-        // Set the color -- be sure to set alpha to something non-zero!
-        marker.color.r = 0.0f;
-        marker.color.g = 1.0f;
-        marker.color.b = 0.0f;
-        marker.color.a = 1.0;
-
-        marker.lifetime = ros::Duration();
-
-        // Publish the marker
-        while (marker_pub.getNumSubscribers() < 1)
-        {
-            if (!ros::ok())
-            {
-                return 0;
-            }
-            ROS_WARN_ONCE("Please create a subscriber to the marker");
-            sleep(1);
-        }
-        marker_pub.publish(marker);
-
-        // Cycle between different shapes
-        switch (shape)
-        {
-            case visualization_msgs::Marker::CUBE:
-                shape = visualization_msgs::Marker::SPHERE;
-                break;
-            case visualization_msgs::Marker::SPHERE:
-                shape = visualization_msgs::Marker::ARROW;
-                break;
-            case visualization_msgs::Marker::ARROW:
-                shape = visualization_msgs::Marker::CYLINDER;
-                break;
-            case visualization_msgs::Marker::CYLINDER:
-                shape = visualization_msgs::Marker::CUBE;
-                break;
-        }
-
-        r.sleep();
-    }
+    display_cube(marker_pub, 0, 0, 0);
 }

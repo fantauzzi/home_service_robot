81492cfe30bb792ef68dfe86a5d56136cc008dcd
fantauzzi <Francesco_Fantauzzi@yahoo.com>
2020-04-24 23:48:10 +0000

    Tested in Udacity's cloud

--- scripts/add_marker.sh
+++ scripts/add_marker.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+echo "Hit Ctrl+C to stop and close everything..."
 PICK_OBJECTS=$(rospack find pick_objects)
 
 PICK_OBJECTS_WORLD=${PICK_OBJECTS}/worlds/training_world.world
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${PICK_OBJECTS_WORLD}" &
-sleep 2
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${PICK_OBJECTS_WORLD}; bash" &
+sleep 5
 
 MAP_FILE=${PICK_OBJECTS}/maps/training_world.yaml
-xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=${MAP_FILE} initial_pose_a:=.0" &
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch map_file:=${MAP_FILE} initial_pose_a:=.0; bash" &
 sleep 2
 
-xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch; bash" &
 
-xterm -e "rosrun add_markers add_markers" &
+xterm -e "rosrun add_markers add_markers; bash" &
 
 wait
--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
+echo "Hit Ctrl+C to stop and close everything..."
 PICK_OBJECTS=$(rospack find pick_objects)
 
 PICK_OBJECTS_WORLD=${PICK_OBJECTS}/worlds/training_world.world
-xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${PICK_OBJECTS_WORLD}" &
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=${PICK_OBJECTS_WORLD}; bash" &
 sleep 2
 
-xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch" &
+xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch; bash" &
 sleep 2
 
-xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch" &
+xterm -e "roslaunch turtlebot_gazebo gmapping_demo.launch; bash" &
 sleep 2
 
-xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch; bash" &
 
 wait
